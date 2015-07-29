7bde7e2d2229fada126bf659e0fb0b74d13c1f12
k1LoW <k1lowxb@gmail.com>
2015-07-29 18:32:59 +0900

    Modify spec

--- spec/type/rds_db_parameter_group_spec.rb
+++ spec/type/rds_db_parameter_group_spec.rb
@@ -17,7 +17,7 @@ Aws.config[:rds] = {
   }
 }
 
-describe rds_db_parameter_group('my-db-parameter-group') do
+describe rds_db_parameter_group('default.mysql5.6') do
   its(:basedir) { should eq '/rdsdbbin/mysql' }
   its(:innodb_buffer_pool_size) { '{DBInstanceClassMemory*3/4}' }
 end

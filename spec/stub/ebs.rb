6cc2e7b3ff9ce9c9b9f9460cf2ca6b38299081f4
k1LoW <k1lowxb@gmail.com>
2015-08-17 23:47:01 +0900

    Add ebs type

--- spec/stub/ebs.rb
+++ spec/stub/ebs.rb
@@ -40,57 +40,6 @@ Aws.config[:ec2] = {
         }
       ]
     },
-    describe_vpcs: {
-      vpcs: [
-        {
-          vpc_id: 'vpc-ab123cde',
-          tags: [
-            {
-              key: 'Name',
-              value: 'my-vpc'
-            }
-          ]
-        }
-      ]
-    },
-    describe_subnets: {
-      subnets: [
-        {
-          state: 'available',
-          vpc_id: 'vpc-ab123cde',
-          subnet_id: 'subnet-1234a567',
-          cidr_block: '10.0.1.0/24',
-          tags: [
-            {
-              key: 'Name',
-              value: 'my-subnet'
-            }
-          ]
-        }
-      ]
-    },
-    describe_addresses: {
-      addresses: [
-        {
-          public_ip: '123.0.456.789',
-          instance_id: 'i-ec12345a'
-        }
-      ]
-    },
-    describe_security_groups: {
-      security_groups: [
-        {
-          group_id: 'sg-1a2b3cd4',
-          group_name: 'my-security-group-name',
-          tags: [
-            {
-              key: 'Name',
-              value: 'my-security-group-tag-name'
-            }
-          ]
-        }
-      ]
-    },
     describe_volumes: {
       volumes: [
         {

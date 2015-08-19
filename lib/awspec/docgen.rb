f550a34c13cc8abf3f848011c4827462b4da38c1
k1LoW <k1lowxb@gmail.com>
2015-08-19 20:41:07 +0900

    Refactor code

--- lib/awspec/docgen.rb
+++ lib/awspec/docgen.rb
@@ -4,11 +4,9 @@ require 'awspec'
 
 Aws.config[:stub_responses] = true
 
-types = %w(
-  ec2 rds rds_db_parameter_group security_group
-  vpc s3 route53_hosted_zone auto_scaling_group subnet
-  route_table ebs
-)
+types = Awspec::Helper::Type::TYPES
+types.delete('base')
+
 links = types.map do |type|
   '[' + type + '](#' + type + ')'
 end
--- lib/awspec/generator.rb
+++ lib/awspec/generator.rb
@@ -1,3 +1,5 @@
+types = Awspec::Helper::Type::TYPES
+
 # Spec
 require 'awspec/generator/spec/ec2'
 require 'awspec/generator/spec/rds'
@@ -6,12 +8,6 @@ require 'awspec/generator/spec/security_group'
 require 'awspec/generator/spec/route53_hosted_zone'
 
 # Doc
-require 'awspec/generator/doc/base'
-types = %w(
-  base ec2 rds rds_db_parameter_group security_group
-  vpc s3 route53_hosted_zone auto_scaling_group subnet
-  route_table ebs
-)
 types.each do |type|
   require 'awspec/generator/doc/' + type
 end

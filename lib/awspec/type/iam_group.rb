2e959b6b43474c562c708691dc2aacb48c8372be
k1LoW <k1lowxb@gmail.com>
2015-09-07 21:53:47 +0900

    Add iam_group type

--- lib/awspec/type/iam_group.rb
+++ lib/awspec/type/iam_group.rb
@@ -2,7 +2,25 @@ module Awspec::Type
   class IamGroup < Base
     def initialize(id)
       super
-      # @id = # @FIXME
+      @resource = find_iam_group(id)
+      @id = @resource[:group_id] if @resource
+    end
+
+    def has_iam_user?(user_id)
+      user = find_iam_user(user_id)
+      return false unless user
+      user_name = user[:user_name]
+      groups = select_iam_group_by_user_name(user_name)
+      groups.find do |group|
+        group.group_id == @id
+      end
+    end
+
+    def has_iam_policy?(policy_id)
+      policies = select_iam_policy_by_group_name(@resource[:group_name])
+      policies.find do |policy|
+        policy.policy_arn == policy_id || policy.policy_name == policy_id
+      end
     end
   end
 end
--- spec/type/iam_group_spec.rb
+++ spec/type/iam_group_spec.rb
@@ -3,4 +3,6 @@ Awspec::Stub.load 'iam_group'
 
 describe iam_group('my-iam-group') do
   it { should exist }
+  it { should have_iam_user('my-iam-user') }
+  it { should have_iam_policy('ReadOnlyAccess') }
 end

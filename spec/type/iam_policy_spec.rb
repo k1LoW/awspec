c949d4c074ed5d4f09fd405d2df27d682f73ecac
hoshinotsuyoshi <guitarpopnot330@gmail.com>
2015-09-08 19:57:03 +0900

    Add some specs for Awspec::Type::IamPolicy :bow:

--- spec/type/iam_policy_spec.rb
+++ spec/type/iam_policy_spec.rb
@@ -4,4 +4,8 @@ Awspec::Stub.load 'iam_policy'
 describe iam_policy('my-iam-policy') do
   it { should exist }
   it { should be_attachable }
+  its(:arn){ should eq 'arn:aws:iam::aws:policy/my-iam-policy' }
+  its(:attachment_count){ should eq 1 }
+  its(:policy_id){ should eq 'PABCDEFGHI123455689' }
+  its(:policy_name){ should eq 'my-iam-policy' }
 end

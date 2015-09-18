88d08b801bc07b3345f3ea71503ca2815b4a864b
k1LoW <k1lowxb@gmail.com>
2015-09-18 21:00:33 +0900

    Update spec

--- spec/type/elasticache_cache_parameter_group_spec.rb
+++ spec/type/elasticache_cache_parameter_group_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 Awspec::Stub.load 'elasticache_cache_parameter_group'
 
-describe elasticache_cache_parameter_group('my-redis-parameter-group') do
+describe elasticache_cache_parameter_group('my-cache-parameter-group') do
   it { should exist }
   its(:activerehashing) { should eq 'yes' }
   its(:client_output_buffer_limit_pubsub_hard_limit) { should eq '33554432' }

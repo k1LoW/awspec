7b45d2f790afebe2dbd8e8c3aa1a02f7c82bbb23
k1LoW <k1lowxb@gmail.com>
2015-10-21 00:14:35 +0900

    Add command `awspec generate cloudwatch_alarm`

--- lib/awspec/command/generate.rb
+++ lib/awspec/command/generate.rb
@@ -16,16 +16,30 @@ module Awspec
       end
     end
 
+    types_for_generate_all = %w(
+      iam_policy cloudwatch_alarm
+    )
+
     desc 'route53_hosted_zone [example.com.]', 'Generate route53_hosted_zone spec from Domain name'
     def route53_hosted_zone(hosted_zone)
       load_secrets
       puts Awspec::Generator::Spec::Route53HostedZone.new.generate_by_domain_name(hosted_zone)
     end
 
-    desc 'iam_policy', 'Generate attached iam_policy spec'
-    def iam_policy
-      load_secrets
-      puts Awspec::Generator::Spec::IamPolicy.new.generate_all
+    types_for_generate_all = %w(
+      iam_policy cloudwatch_alarm
+    )
+
+    types_for_generate_all.each do |type|
+      if type == 'iam_policy'
+        desc 'iam_policy', 'Generate attached iam_policy spec'
+      else
+        desc type, "Generate #{type} spec"
+      end
+      define_method type do
+        load_secrets
+        eval "puts Awspec::Generator::Spec::#{type.camelize}.new.generate_all"
+      end
     end
 
     no_commands do

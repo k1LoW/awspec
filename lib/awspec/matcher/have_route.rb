c10de376a03bf03af809841d72e3c58638178cf9
k1LoW <k1lowxb@gmail.com>
2015-11-16 22:38:57 +0900

    Support deprecated route_table usage

--- lib/awspec/matcher/have_route.rb
+++ lib/awspec/matcher/have_route.rb
@@ -1,6 +1,12 @@
+require 'ipaddr'
+
 RSpec::Matchers.define :have_route do |destination|
   match do |route_table|
-    @destination = destination
+    if !is_ipaddr?(destination) && @use_destination
+      @gateway_id = destination
+    else
+      @destination = destination
+    end
     route_table.has_route?(@destination, @gateway_id, @instance_id)
   end
 
@@ -10,6 +16,16 @@ RSpec::Matchers.define :have_route do |destination|
   end
 
   chain :destination do |dest|
+    puts '!!! `destination is deprecated. see https://github.com/k1LoW/awspec/pull/65 !!!'
+    @use_destination = true
     @destination = dest
+  end
+
+  private
+
+  def is_ipaddr?(str)
+    IPAddr.new(str)
+  rescue
+    false
   end
 end

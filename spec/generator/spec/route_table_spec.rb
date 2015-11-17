3f3b92e6cbe245616cb8b3959bc399f14b1e44aa
k1LoW <k1lowxb@gmail.com>
2015-11-17 19:31:08 +0900

    Update Awspec::Generator::Spec::RouteTable

--- spec/generator/spec/route_table_spec.rb
+++ spec/generator/spec/route_table_spec.rb
@@ -13,6 +13,7 @@ describe route_table('my-route-table') do
   it { should have_route('10.0.0.0/16').target(gateway: 'local') }
   it { should have_route('0.0.0.0/0').target(gateway: 'igw-1ab2345c') }
   it { should have_route('192.168.1.0/24').target(instance: 'my-ec2') }
+  it { should have_subnet('my-subnet') }
 end
 EOF
     expect(route_table.generate_by_vpc_id('my-vpc').to_s.gsub(/\n/, "\n")).to eq spec

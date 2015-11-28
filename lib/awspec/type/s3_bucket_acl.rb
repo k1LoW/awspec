dc74be22f8921f307d71a42b8c8474c937512ab0
kirikiriyamama <kirikiriyamama@icloud.com>
2015-11-29 01:44:27 +0900

    Follow cording standards

--- lib/awspec/type/s3_bucket_acl.rb
+++ lib/awspec/type/s3_bucket_acl.rb
@@ -7,17 +7,17 @@ module Awspec::Type
     end
 
     def has_grant?(conditions)
-      evaluator = ->(obj, attr, val){
+      evaluator = lambda do |obj, attr, val|
         if val.is_a?(Hash)
           o = obj.send(attr)
-          val.all?{ |a, v| evaluator.call(o, a, v) }
+          val.all? { |a, v| evaluator.call(o, a, v) }
         else
           obj.send(attr) == val
         end
-      }
+      end
 
       @resource.grants.find do |grant|
-        conditions.all?{ |attr, val| evaluator.call(grant, attr, val) }
+        conditions.all? { |attr, val| evaluator.call(grant, attr, val) }
       end
     end
   end

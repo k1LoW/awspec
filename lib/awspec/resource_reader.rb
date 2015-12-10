8835870e0e3016cab75c852fea9e0bf58cbbf7da
Uchio KONDO <udzura@udzura.jp>
2015-12-10 21:15:35 +0900

    Split black listing method

--- lib/awspec/resource_reader.rb
+++ lib/awspec/resource_reader.rb
@@ -1,17 +1,14 @@
 module Awspec
-  class ResourceReader
+  module BlackListForwardable
     class CalledMethodInBlackList < StandardError
     end
 
-    def initialize(resource)
-      @resource = resource
-    end
-
-    def method_missing(name, *args)
+    def method_missing_via_black_list(name, delegate_to: nil)
+      raise(ArguementError, "delegate_to: must be specified") unless delegate_to
       if match_black_list?(name)
         raise CalledMethodInBlackList, "Method call #{name.inspect} is black-listed"
       else
-        attr = @resource.send(name)
+        attr = delegate_to.send(name)
         case attr
         when Aws::Resources::Resource
           ResourceReader.new(attr)
@@ -34,6 +31,18 @@ module Awspec
 
     def match_black_list?(name)
       !!(BLACK_LIST_RE =~ name)
+    end
+  end
+
+  class ResourceReader
+    include BlackListForwardable
+
+    def initialize(resource)
+      @resource = resource
+    end
+
+    def method_missing(name)
+      method_missing_via_black_list(name, delegate_to: @resource)
     end
   end
 end

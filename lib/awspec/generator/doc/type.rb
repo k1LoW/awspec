6c5087163cca54d8b286859dd65fae6310a0bbf8
Arthur Rimbun <arthur.rimbun@gmail.com>
2016-07-15 19:56:52 +1000

    Remove unnecessary line.

--- lib/awspec/generator/doc/type.rb
+++ lib/awspec/generator/doc/type.rb
@@ -17,7 +17,6 @@ EOF
         doc = ERB.new(header, nil, '-').result(binding)
 
         types.map do |type|
-          Awspec::Generator::Doc::Type::Cloudtrail.new
           doc += eval "Awspec::Generator::Doc::Type::#{type.camelize}.new.generate_doc"
         end
         doc.sub(/\n*\z/, '')

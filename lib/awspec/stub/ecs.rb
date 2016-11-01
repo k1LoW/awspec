f77b5e4d017a8d47e6035de36bdda2aae96a096e
Joe Khoobyar <fourheads@gmail.com>
2016-11-01 14:35:09 -0400

    fix specs

--- lib/awspec/stub/ecs.rb
+++ lib/awspec/stub/ecs.rb
@@ -3,9 +3,13 @@ Aws.config[:ecs] = {
     describe_clusters: {
       clusters: [
         {
-          cluster_arn: "arn:aws:ecs:us-east-1:123456789012:cluster/my-ecs-cluster", 
-          cluster_name: "my-ecs-cluster", 
-          status: "ACTIVE",
+          cluster_arn:                           "arn:aws:ecs:us-east-1:123456789012:cluster/my-ecs-cluster", 
+          cluster_name:                          "my-ecs-cluster", 
+          status:                                "ACTIVE",
+          registered_container_instances_count:  0,
+          pending_tasks_count:                   0,
+          running_tasks_count:                   0,
+          active_services_count:                 0,
         }
       ],
       failures: [

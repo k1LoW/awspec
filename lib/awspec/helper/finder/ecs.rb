39e7f98b8f60d8c8ee1589a316dfbac507eb04eb
Joe Khoobyar <fourheads@gmail.com>
2016-11-01 16:21:04 -0400

    more support for ECS clusters

--- lib/awspec/helper/finder/ecs.rb
+++ lib/awspec/helper/finder/ecs.rb
@@ -1,11 +1,27 @@
 module Awspec::Helper
   module Finder
     module Ecs
-      def find_ecs_cluster(name)
-        # cluster_name
-        res = ecs_client.describe_clusters({ clusters: [name] })
+      def find_ecs_cluster(cluster_name)
+        res = ecs_client.describe_clusters(clusters: [cluster_name])
           
         res.clusters.first if res.clusters.count == 1
+      end
+      
+      def list_ecs_container_instances(cluster_name)
+        req = {cluster: cluster_name}
+        container_instances = []
+        loop do
+          res = ecs_client.list_container_instances(req)
+          container_instances.push(*res.container_instances)
+          break if res.next_token.nil?
+          req[:next_token] = res.next_token
+        end
+        container_instances
+      end
+      
+      def find_ecs_container_instances(cluster_name, container_instances)
+        res = ecs_client.describe_container_instances(cluster: cluster_name, container_instances: container_instances)
+        res.container_instances if res.container_instances
       end
     end
   end
--- lib/awspec/type/ecs_cluster.rb
+++ lib/awspec/type/ecs_cluster.rb
@@ -14,5 +14,22 @@ module Awspec::Type
     def id
       @id ||= resource_via_client.cluster_name if resource_via_client
     end
+    
+    def active?
+      resource_via_client.status == 'ACTIVE'
+    end
+    
+    def inactive?
+      resource_via_client.status == 'INACTIVE'
+    end
+    
+    def container_instances
+      return @container_instances if @container_instances
+      @container_instances = list_container_instances
+      unless @container_instances.empty?
+        @container_instances = find_ecs_container_instances(id, @container_instances)
+      end
+      @container_instances
+    end
   end
 end

b112664c6df07ba58e0836d4522b755770f8346f
Ringo De Smet <ringo@de-smet.name>
2017-02-09 09:45:48 +0100

    Fixing the rubocop style issue.

--- lib/awspec/type/security_group.rb
+++ lib/awspec/type/security_group.rb
@@ -98,7 +98,7 @@ module Awspec::Type
       ret = permission.user_id_group_pairs.select do |sg|
         # Compare the sg group_name if the remote group is in another account.
         # find_security_group call doesn't return info on a remote security group.
-        if (sg.user_id != nil) && (sg.user_id != resource_via_client.owner_id)
+        if !sg.user_id.nil? && (sg.user_id != resource_via_client.owner_id)
           next (sg.group_name == cidr) || (sg.group_id == cidr)
         end
         next true if sg.group_id == cidr

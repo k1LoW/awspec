96c5a35912bdcabc02678726b5148627e96b6d0b
k1LoW <k1lowxb@gmail.com>
2017-02-14 10:28:33 +0900

    Change ecs_contaier_instance API

--- lib/awspec/generator/doc/type/ecs_container_instance.rb
+++ lib/awspec/generator/doc/type/ecs_container_instance.rb
@@ -4,9 +4,13 @@ module Awspec::Generator
       class EcsContainerInstance < Base
         def initialize
           super
+          @type_name = 'ECS Container Instance'
           @type = Awspec::Type::EcsContainerInstance.new('my-ecs-container-instance')
-          @matchers = []
-          @ignore_matchers = []
+          @ret = @type.resource_via_client
+          @matchers = [
+            Awspec::Type::EcsContainerInstance::STATES.map { |state| 'be_' + state.downcase }.join(', ')
+          ]
+          @ignore_matchers = Awspec::Type::EcsContainerInstance::STATES.map { |state| 'be_' + state.downcase }
           @describes = []
         end
       end
--- lib/awspec/shared_context.rb
+++ lib/awspec/shared_context.rb
@@ -1,3 +1,5 @@
+require 'pp'
+
 shared_context 'region', :region do
   before do |example|
     region = example.metadata[:region]
@@ -7,5 +9,15 @@ shared_context 'region', :region do
 
   after do
     Aws.config[:region] = @_region
+  end
+end
+
+shared_context 'cluster_name', :cluster_name do
+  before do |example|
+    cluster_name = example.metadata[:cluster_name]
+    example.metadata[:described_class].cluster_name = cluster_name
+  end
+
+  after do
   end
 end

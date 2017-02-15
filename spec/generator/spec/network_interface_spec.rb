e288b6c016aa0a506b4f8ec1f1415802ab13e5a5
k1LoW <k1lowxb@gmail.com>
2017-02-15 12:38:38 +0900

    Fix network_interface

--- spec/generator/spec/network_interface_spec.rb
+++ spec/generator/spec/network_interface_spec.rb
@@ -10,7 +10,7 @@ describe 'Awspec::Generator::Spec::NetworkInterface' do
 describe network_interface('eni-12ab3cde') do
   it { should exist }
   it { should be_in_use }
-  it { should be_attached_to('my-ec2').as_eth0 }
+  it { should be_attached_to('my-ec2').as_eth1 }
   it { should belong_to_vpc('my-vpc') }
   it { should belong_to_subnet('my-subnet') }
   it { should have_private_ip_address('10.0.1.1').primary }

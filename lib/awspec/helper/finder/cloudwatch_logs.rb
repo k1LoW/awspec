cffc970c0ed837f807a723b26575bc8ec86ccdc8
Yohei Kawahara <kawahara@cloudpack.jp>
2017-03-14 07:42:51 +0900

    fix method name and check log-group-name and filter-name

--- lib/awspec/helper/finder/cloudwatch_logs.rb
+++ lib/awspec/helper/finder/cloudwatch_logs.rb
@@ -2,25 +2,33 @@ module Awspec::Helper
   module Finder
     module CloudwatchLogs
       def find_cloudwatch_logs_group(id)
-        cloudwatch_logs_client.describe_log_groups({ log_group_name_prefix: id }).log_groups.last
+        cloudwatch_logs_client.describe_log_groups({
+                                                     log_group_name_prefix: id
+                                                   }).log_groups.find do |log_group|
+          log_group.log_group_name == id
+        end
       end
 
-      def find_cloudwatch_logs_stream(id)
+      def find_cloudwatch_logs_stream_by_log_group_name(id)
         cloudwatch_logs_client.describe_log_streams({ log_group_name: id }).log_streams.last
       end
 
-      def find_cloudwatch_logs_metric_fileter(id, filter_name)
+      def find_cloudwatch_logs_metric_fileter_by_log_group_name(id, filter_name_prefix)
         cloudwatch_logs_client.describe_metric_filters({
                                                          log_group_name: id,
-                                                         filter_name_prefix: filter_name
-                                                       }).metric_filters.last
+                                                         filter_name_prefix: filter_name_prefix
+                                                       }).metric_filters.find do |filter|
+          filter.filter_name == filter_name_prefix
+        end
       end
 
-      def find_cloudwatch_logs_subscription_fileter(id, filter_name)
+      def find_cloudwatch_logs_subscription_fileter_by_log_group_name(id, filter_name_prefix)
         cloudwatch_logs_client.describe_subscription_filters({
                                                                log_group_name: id,
-                                                               filter_name_prefix: filter_name
-                                                             }).subscription_filters.last
+                                                               filter_name_prefix: filter_name_prefix
+                                                             }).subscription_filters.find do |filter|
+          filter.filter_name == filter_name_prefix
+        end
       end
     end
   end

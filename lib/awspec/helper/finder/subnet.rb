7e2691d04ebeed0cb5d7ccaed45870719bfbf993
tenbo07 <tenbo07@gmail.com>
2017-05-17 15:15:55 +0900

    find subnet from cidr

--- lib/awspec/helper/finder/subnet.rb
+++ lib/awspec/helper/finder/subnet.rb
@@ -1,16 +1,23 @@
 module Awspec::Helper
   module Finder
     module Subnet
-      def find_subnet(subnet_id)
+      def filter_type_matcher(search_key)
+        deafult = 'tag:Name'
+        case search_key
+        when /subnet-[a-zA-Z0-9]{8}/
+          return 'subnet-id'
+        when %r{[1-9][0-9]{1,2}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}//[0-9]{1,3}}
+          return 'cidrBlock'
+        else
+          return deafult
+        end
+      end
+
+      def find_subnet(search_key)
         res = ec2_client.describe_subnets({
-                                            filters: [{ name: 'subnet-id', values: [subnet_id] }]
+                                            filters: [{ name: filter_type_matcher(search_key), values: [search_key] }]
                                           })
-        resource = res.subnets.single_resource(subnet_id)
-        return resource if resource
-        res = ec2_client.describe_subnets({
-                                            filters: [{ name: 'tag:Name', values: [subnet_id] }]
-                                          })
-        res.subnets.single_resource(subnet_id)
+        res.subnets.single_resource(search_key)
       end
 
       def select_subnet_by_vpc_id(vpc_id)

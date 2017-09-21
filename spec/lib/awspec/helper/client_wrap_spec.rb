b7032919018d967c3162c675f9c998e23e204195
Craig Monson <craig@malachiarts.com>
2017-09-21 18:41:18 -0400

    Fixing some bugs, and rubocop complaints.

--- spec/lib/awspec/helper/client_wrap_spec.rb
+++ spec/lib/awspec/helper/client_wrap_spec.rb
@@ -3,11 +3,10 @@ require 'spec_helper'
 # used to test the method_missing functionality #
 module RSpec::Mocks
   module Errors
-    class RequestLimitExceeded < Exception
+    class RequestLimitExceeded < RuntimeError
     end
   end
 end
-
 
 describe Awspec::Helper::ClientWrap do
   let(:subj) { Awspec::Helper::ClientWrap }
@@ -55,7 +54,7 @@ describe Awspec::Helper::ClientWrap do
 
     context 'given no client' do
       it 'should raise an ArgumentError' do
-        expect { subj.new() }.to raise_error(ArgumentError, 'Client can not be nil')
+        expect { subj.new }.to raise_error(ArgumentError, 'Client can not be nil')
       end
     end
 
@@ -68,25 +67,29 @@ describe Awspec::Helper::ClientWrap do
 
   describe '#method_missing (protected)' do
     context 'given the client does not raise ::RequestLimitExceeded error' do
-      let(:client) { subj.new(Hash.new()) }
+      let(:client) { subj.new({}) }
 
       it 'should pass the method through to the client' do
         expect(client.empty?).to be true
       end
 
       it 'should raise exceptions not caught' do
-        expect{ client.foo }.to raise_error(NoMethodError)
+        expect { client.foo }.to raise_error(NoMethodError)
       end
     end
 
     context 'given the client raises ::RequestLimitExceeded error' do
-      let(:client) { double "fake" }
+      let(:client) { double 'fake' }
 
       it 'should be called multiple times in a sleep loop, and re-raise the exception if it is not cleared' do
         calls = 0
-        allow(client).to receive(:test_me) { calls += 1; raise RSpec::Mocks::Errors::RequestLimitExceeded }
+        allow(client).to receive(:test_me) do
+          calls += 1
+          raise RSpec::Mocks::Errors::RequestLimitExceeded
+        end
+
         foo = subj.new(client, backoff_limit: 1)
-        expect {foo.test_me}.to raise_error(RSpec::Mocks::Errors::RequestLimitExceeded)
+        expect { foo.test_me }.to raise_error(RSpec::Mocks::Errors::RequestLimitExceeded)
         expect(foo.backoff).to eq(2.5)
         expect(foo.iteration).to eq(3)
       end
@@ -94,12 +97,15 @@ describe Awspec::Helper::ClientWrap do
       it 'return as expected once the error is cleared' do
         calls = 0
         res = ''
-        allow(client).to receive(:test_me) { calls += 1; raise RSpec::Mocks::Errors::RequestLimitExceeded, "Fail" if calls == 1; 'done' }
+        allow(client).to receive(:test_me) do
+          calls += 1
+          raise RSpec::Mocks::Errors::RequestLimitExceeded, 'Fail' if calls == 1
+          'done'
+        end
         foo = subj.new(client, backoff_limit: 1)
-        expect {res = foo.test_me}.to_not raise_error()
+        expect { res = foo.test_me }.to_not raise_error
         expect(res).to eq 'done'
       end
-
     end
   end
 end

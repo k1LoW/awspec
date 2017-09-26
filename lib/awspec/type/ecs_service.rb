aa6d422495b770b114abafcc1b6494f534a69268
k-kayashima <k-kayashima@navitime.co.jp>
2017-09-26 14:44:42 +0900

    add cluster-name

--- lib/awspec/type/ecs_service.rb
+++ lib/awspec/type/ecs_service.rb
@@ -1,17 +1,23 @@
 module Awspec::Type
   class EcsService < ResourceBase
+    attr_accessor :cluster
     def initialize(service)
       super
       @display_name = service
     end
 
     def resource_via_client
-      @resource_via_client ||= find_ecs_service(@display_name)
+      @resource_via_client ||= find_ecs_service(cluster, @display_name)
     end
 
     def id
       @id ||= resource_via_client.service_name if resource_via_client
     end
+
+    def cluster
+      @cluster || 'default'
+    end
+
 
     STATES = %w(ACTIVE DRAINING INACTIVE)
 

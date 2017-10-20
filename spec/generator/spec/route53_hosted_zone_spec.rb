2cf354fbff7d3a4831c143b7445139072f940108
Ryuzo Yamamoto <ryuzo.yamamoto@gmail.com>
2017-10-20 09:01:19 -0400

    Add CAA record support in route53_hosted_zone
    http://docs.aws.amazon.com/Route53/latest/DeveloperGuide/ResourceRecordTypes.html#CAAFormat
    http://docs.aws.amazon.com/acm/latest/userguide/setup-caa.html

--- spec/generator/spec/route53_hosted_zone_spec.rb
+++ spec/generator/spec/route53_hosted_zone_spec.rb
@@ -9,7 +9,7 @@ describe 'Awspec::Generator::Spec::Route53HostedZone' do
     spec = <<-'EOF'
 describe route53_hosted_zone('example.com.') do
   it { should exist }
-  its(:resource_record_set_count) { should eq 6 }
+  its(:resource_record_set_count) { should eq 8 }
   it { should have_record_set('example.com.').a('123.456.7.890').ttl(3600) }
   it { should have_record_set('*.example.com.').cname('example.com').ttl(3600) }
   it { should have_record_set('example.com.').mx('10 mail.example.com').ttl(3600) }
@@ -20,6 +20,11 @@ ns-6789.awsdns-01.org.
 ns-2345.awsdns-67.co.uk.
 ns-890.awsdns-12.com.').ttl(172800) }
   it { should have_record_set('s3.example.com.').alias('s3-website-us-east-1.amazonaws.com.', 'Z2ABCDEFGHIJKL') }
+  it { should have_record_set('example.com.').caa('0 issue "amazon.com"
+0 issue "amazontrust.com"
+0 issue "awstrust.com"
+0 issuewild "amazonaws.com"
+0 iodef "mailto:support@example.com"').ttl(600) }
 end
 EOF
     expect(route53_hosted_zone.generate_by_domain_name('example.com.').to_s.gsub(/\n/, "\n")).to eq spec

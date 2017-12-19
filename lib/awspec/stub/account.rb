d40fd3475d08028d86172187a67c90155dfc4ac2
k1LoW <k1lowxb@gmail.com>
2017-12-20 08:27:25 +0900

    docgen

--- lib/awspec/stub/account.rb
+++ lib/awspec/stub/account.rb
@@ -117,7 +117,8 @@ Aws.config[:lambda] = {
         total_code_size: 80_530_636_800,
         code_size_unzipped: 262_144_000,
         code_size_zipped: 52_428_800,
-        concurrent_executions: 200
+        concurrent_executions: 200,
+        unreserved_concurrent_executions: 50
       },
       account_usage: {
         total_code_size: 2_034_651_562,

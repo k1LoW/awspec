6a39119f3dcbc20337f968cdd283a4b8c4142321
inokappa <inokara@gmail.com>
2018-02-01 23:26:57 +0900

    Add DB Parameter Group Generator

---
+++ lib/awspec/generator/spec/rds_db_cluster_parameter_group.rb
@@ -0,0 +1,26 @@
+module Awspec::Generator
+  module Spec
+    class RdsDbClusterParameterGroup
+      include Awspec::Helper::Finder
+      def generate_by_paramater_group(paramater_group)
+        @paramater_group = paramater_group
+        res = select_all_rds_db_cluster_parameters(@paramater_group)
+        # res = rds_client.describe_db_cluster_parameters({
+        #                                                   db_cluster_parameter_group_name: @paramater_group
+        #                                                 })
+        ERB.new(db_cluster_parameter_group_template, nil, '-').result(binding).gsub(/^\n/, '')
+      end
+
+      def db_cluster_parameter_group_template
+        template = <<-'EOF'
+describe rds_db_cluster_parameter_group('<%= @paramater_group %>') do
+<% res.each do |key, value| %>
+  its('<%= key %>') { should eq '<%= value %>' }
+<% end %>
+end
+EOF
+        template
+      end
+    end
+  end
+end

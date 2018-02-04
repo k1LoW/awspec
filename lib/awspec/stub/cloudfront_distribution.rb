27227d8a4b7864ebc6546cecc4483c504f248f72
inokappa <inokara@gmail.com>
2018-02-04 13:28:39 +0900

    Add tests

--- lib/awspec/stub/cloudfront_distribution.rb
+++ lib/awspec/stub/cloudfront_distribution.rb
@@ -176,7 +176,27 @@ Aws.config[:cloudfront] = {
               },
               custom_error_responses: {
                 quantity: 0,
-                items: []
+                items:
+                  [
+                    {
+                      error_code: 400,
+                      response_page_path: '/path/to/400.html',
+                      response_code: '400',
+                      error_caching_min_ttl: 60
+                    },
+                    {
+                      error_code: 403,
+                      response_page_path: '/path/to/403.html',
+                      response_code: '403',
+                      error_caching_min_ttl: 60
+                    },
+                    {
+                      error_code: 500,
+                      response_page_path: '',
+                      response_code: '',
+                      error_caching_min_ttl: 60
+                    }
+                  ]
               },
               comment: 'cf-s3-origin-hosting.dev.example.com',
               price_class: 'PriceClass_200',
--- spec/type/cloudfront_distribution_spec.rb
+++ spec/type/cloudfront_distribution_spec.rb
@@ -22,4 +22,20 @@ end
 
 describe cloudfront_distribution('123456789zyxw.cloudfront.net') do
   it { should exist }
+  it do
+    should have_custom_response_error_code(400)
+      .error_caching_min_ttl(60)
+      .response_page_path('/path/to/400.html')
+      .response_code(400)
+  end
+  it do
+    should have_custom_response_error_code(403)
+      .error_caching_min_ttl(60)
+      .response_page_path('/path/to/403.html')
+      .response_code('403')
+  end
+  it do
+    should have_custom_response_error_code(500)
+      .error_caching_min_ttl(60)
+  end
 end

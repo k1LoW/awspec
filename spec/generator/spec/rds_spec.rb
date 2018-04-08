938f8f5a4f8827df84359ad7c23fd7c086d69427
htnosm <25860168+htnosm@users.noreply.github.com>
2018-04-08 19:44:18 +0900

    Add generate for parameter_apply_status and option_group status

--- spec/generator/spec/rds_spec.rb
+++ spec/generator/spec/rds_spec.rb
@@ -17,10 +17,10 @@ describe rds('my-rds') do
   it { should have_security_group('group-name-sg') }
   it { should belong_to_vpc('my-vpc') }
   it { should belong_to_db_subnet_group('my-db-subnet-group') }
-  it { should have_db_parameter_group('default.mysql5.6') }
-  it { should have_db_parameter_group('custom.mysql5.6') }
-  it { should have_option_group('default:mysql-5-6') }
-  it { should have_option_group('custom:mysql-5-6') }
+  it { should have_db_parameter_group('default.mysql5.6').parameter_apply_status('pending-reboot') }
+  it { should have_db_parameter_group('custom.mysql5.6').parameter_apply_status('in-sync') }
+  it { should have_option_group('default:mysql-5-6').status('in-sync') }
+  it { should have_option_group('custom:mysql-5-6').status('in-sync') }
 end
 EOF
     expect(rds.generate_by_vpc_id('my-vpc').to_s.gsub(/\n/, "\n")).to eq spec

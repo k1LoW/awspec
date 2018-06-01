7133380bd7f7bd9b63abcc952c331ea283366d7d
Joe Khoobyar <joe@khoobyar.name>
2018-06-01 17:07:14 -0400

    fix KMS key finding by alias

--- lib/awspec/helper/finder/kms.rb
+++ lib/awspec/helper/finder/kms.rb
@@ -8,14 +8,30 @@ module Awspec::Helper
       end
 
       def find_kms_key_by_alias(key_alias_name)
-        key = kms_client.list_aliases.aliases.find do |key_alias|
-          key_alias.alias_name == "alias/#{key_alias_name}"
+        alias_name = key_alias_name.start_with?('alias/') ? key_alias_name : "alias/#{key_alias_name}"
+        found = nil
+        next_marker = nil
+
+        loop do
+          res = kms_client.list_aliases(marker: next_marker, limit: 100)
+          found = res.aliases.find { |key_alias| key_alias.alias_name == alias_name }
+          (found.nil? && next_marker = res.next_marker) || break
         end
-        find_kms_key(key.target_key_id)
+        
+        find_kms_key(found.target_key_id) if found
       end
 
       def select_all_kms_aliases
-        kms_client.list_aliases.aliases
+        all_aliases = []
+        next_marker = nil
+
+        loop do
+          res = kms_client.list_aliases(marker: next_marker, limit: 100)
+          all_aliases.concat(res.aliases)
+          next_marker = res.next_marker || break
+        end
+        
+        all_aliases
       end
     end
   end

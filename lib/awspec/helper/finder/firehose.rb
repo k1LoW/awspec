83e52f8c067ec3c00ba8ce26475584ef13cacf87
Mitko <mitkoo@gmail.com>
2018-06-15 11:43:12 +1000

    Removed limit, api is a bit confusing since you can only have one destination anyway... :limit (Integer) — The limit on the number of destinations to return. You can have one destination per delivery stream.

--- lib/awspec/helper/finder/firehose.rb
+++ lib/awspec/helper/finder/firehose.rb
@@ -2,11 +2,11 @@ module Awspec::Helper
   module Finder
     module Firehose
       def find_delivery_stream(name)
-        res = firehose_client.describe_delivery_stream({
-                                                         delivery_stream_name: name,
-                                                         limit: 1
-                                                       })
-        res.delivery_stream_description
+        firehose_client
+          .describe_delivery_stream({
+                                      delivery_stream_name: name
+                                    })
+          .delivery_stream_description
       end
     end
   end

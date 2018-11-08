b279a6d49f92e66a41f8edbbd304e69cdd46c78d
Alceu Rodrigues de Freitas Junior <cin_afreitas@uolinc.com>
2018-11-08 10:16:32 -0200

    Fixed Rubocop issues

--- lib/awspec/matcher/include_subscribed.rb
+++ lib/awspec/matcher/include_subscribed.rb
@@ -1,5 +1,5 @@
 RSpec::Matchers.define :include_subscribed do |subscriber_arn|
   match do |sns_topic|
-    sns_topic.list_subscriptions.has_key?(subscriber_arn.to_sym)
+    sns_topic.list_subscriptions.key?(subscriber_arn.to_sym)
   end
 end

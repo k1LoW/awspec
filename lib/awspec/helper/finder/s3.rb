f3691950634857da8148174bc6492cf73d6f1daa
Tatsuya Hoshino <tatsuya7.hoshino7@gmail.com>
2019-02-11 21:54:54 +0900

    Remove unused variables

--- lib/awspec/helper/finder/s3.rb
+++ lib/awspec/helper/finder/s3.rb
@@ -2,8 +2,7 @@ module Awspec::Helper
   module Finder
     module S3
       def find_bucket(id)
-        res = s3_client.list_buckets
-        ret = s3_client.list_buckets[:buckets].find do |bucket|
+        s3_client.list_buckets[:buckets].find do |bucket|
           bucket.name == id
         end
       end

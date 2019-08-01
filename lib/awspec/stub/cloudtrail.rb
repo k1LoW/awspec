0ad187660ef1bcee8cdf6f9eb72c35f79c035e80
tjjjwxzq <aquietimmanence@gmail.com>
2019-08-01 12:20:21 +0800

    Patch CloudTrail resource to respond to has_tag? by fetching tags through list_tags sdk method

--- lib/awspec/stub/cloudtrail.rb
+++ lib/awspec/stub/cloudtrail.rb
@@ -6,12 +6,30 @@ Aws.config[:cloudtrail] = {
           name: 'my-trail',
           include_global_service_events: true,
           is_multi_region_trail: true,
-          log_file_validation_enabled: true
+          log_file_validation_enabled: true,
+          trail_arn: 'my-trail-arn'
         }
       ]
     },
     get_trail_status: {
       is_logging: true
+    },
+    list_tags: {
+      resource_tag_list: [
+        {
+          resource_id: 'my-trail-arn',
+          tags_list: [
+            {
+              key: 'key_one',
+              value: 'value_one'
+            },
+            {
+              key: 'key_two',
+              value: 'value_two'
+            }
+          ]
+        }
+      ]
     }
   }
 }

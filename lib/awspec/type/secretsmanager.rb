85da15939f2e483a559bcf0180e52dec29fb884e
tjjjwxzq <aquietimmanence@gmail.com>
2019-08-17 17:26:11 +0800

    Add have_tag matcher to secretsmanager resource

--- lib/awspec/type/secretsmanager.rb
+++ lib/awspec/type/secretsmanager.rb
@@ -1,6 +1,7 @@
 module Awspec::Type
   class Secretsmanager < ResourceBase
     aws_resource Aws::SecretsManager
+    tags_allowed
 
     def resource_via_client
       @resource_via_client ||= find_secret(@display_name)

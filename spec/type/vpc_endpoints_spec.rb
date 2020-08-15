8d6510b1b9b202b3ce7d62449bc793789993cee3
Andy Moore <andy@impulsed.net>
2020-08-16 00:33:12 +0100

    update spec and stub

--- spec/type/vpc_endpoints_spec.rb
+++ spec/type/vpc_endpoints_spec.rb
@@ -1,9 +1,18 @@
 require 'spec_helper'
 Awspec::Stub.load 'vpc_endpoints'
-describe vpc_endpoints('my-vpcendpoint') do
+
+describe vpc_endpoints('vpce-abc123') do
+  it { should exist }
+  it { should be_available }
+  it { should belong_to_vpc('vpc-12345678') }
+  it { should have_route_table('rtb-0123456789abcdefg') }
+  its(:vpc_endpoint_type) { should eq 'Gateway' }
+end
+
+describe vpc_endpoints('vpce-123456789abcdefgh') do
   it { should exist }
   it { should be_available }
   it { should belong_to_vpc('vpc-abc123') }
-  it { should have_subnet('my-subnet') }
-  it { should have_route_table('my-subnet') }
+  it { should have_subnet('subnet-abc123') }
+  its(:vpc_endpoint_type) { should eq 'Interface' }
 end

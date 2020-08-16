690bec2775942e7ccaa48f2d2eb6946fc4b10e74
Andy Moore <andy@impulsed.net>
2020-08-16 19:49:05 +0100

    make this work in tests

--- lib/awspec/helper/finder/vpc_endpoints.rb
+++ lib/awspec/helper/finder/vpc_endpoints.rb
@@ -2,10 +2,13 @@ module Awspec::Helper
   module Finder
     module VpcEndpoints
       def find_vpc_endpoint(id)
-        res = ec2_client.describe_vpc_endpoints({
-                                         filters: [{ name: 'vpc-endpoint-id', values: [id] }]
-                                       })
-        resource = res.vpc_endpoints.single_resource(id)
+        res = ec2_client.describe_vpc_endpoints({vpc_endpoint_ids: [id]})
+
+        ret = res.vpc_endpoints.select do |vpce|
+          vpce.vpc_endpoint_id == id
+        end
+
+        ret.single_resource(id)
       end
     end
   end

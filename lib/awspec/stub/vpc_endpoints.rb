6300ee52998db5fb5b86b369474c704ea1bfc9cb
Andy Moore <andy@impulsed.net>
2020-08-16 20:04:55 +0100

    fix linting

--- lib/awspec/stub/vpc_endpoints.rb
+++ lib/awspec/stub/vpc_endpoints.rb
@@ -38,11 +38,16 @@ Aws.config[:ec2] = {
                            '"Resource": "*"}]}',
           route_table_ids: [],
           subnet_ids: ['subnet-abc123'],
-          groups: [{group_id: "sg-abc123", group_name: "default"}],
+          groups: [{ group_id: 'sg-abc123', group_name: 'default' }],
           private_dns_enabled: true,
           requester_managed: false,
           network_interface_ids: ['eni-06f28a949cb88b84c'],
-          dns_entries: [{dns_name: "vpce-05907f23265b25f20-wwafshom.codebuild.eu-west-1.vpce.amazonaws.com", hosted_zone_id: "Z38GZ743OKFT7T"}],
+          dns_entries: [
+            {
+              dns_name: 'vpce-05907f23265b25f20-wwafshom.codebuild.eu-west-1.vpce.amazonaws.com',
+              hosted_zone_id: 'Z38GZ743OKFT7T'
+            }
+          ],
           creation_timestamp: Time.new(2020, 8, 13, 9, 00, 00, '+00:00'),
           tags: [
             {

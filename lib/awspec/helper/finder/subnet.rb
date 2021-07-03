d341742b31d213ab84e704c023ad1142ba126dd3
Alceu Rodrigues de Freitas Junior <glasswalk3r@yahoo.com.br>
2021-07-03 13:21:47 -0300

    fix: returning a single Aws::EC2::Types::Subnet

--- lib/awspec/helper/finder/subnet.rb
+++ lib/awspec/helper/finder/subnet.rb
@@ -30,7 +30,7 @@ module Awspec::Helper
         end
 
         def has_subnet?(subnet_id_symbol)
-          @subnet_ids.has?(subnet_id_symbol)
+          @subnet_ids.key?(subnet_id_symbol)
         end
 
         def subnet_by_cidr(cidr_symbol)
@@ -44,30 +44,36 @@ module Awspec::Helper
         def is_cidr?(subnet_id)
           @ip_matcher.match(subnet_id)
         end
+
+        def empty?
+          @subnet_ids.empty?
+        end
       end
 
       def find_subnet(subnet_id)
-        res = ec2_client.describe_subnets
         cache = SubnetCache.instance
-        subnet_ids = []
 
-        res.subnets.each do |sub|
-          cache.add_by_cidr(sub.cidr_block, sub.subnet_id)
-          cache.add_subnet(sub)
-          next if sub.tags.empty?
+        if cache.empty?
+          res = ec2_client.describe_subnets
 
-          sub.tags.each do |tag|
-            if tag[:key].eql?('Name')
-              cache.add_by_tag(tag[:value], sub.subnet_id)
-              break
+          res.subnets.each do |sub|
+            cache.add_by_cidr(sub.cidr_block, sub.subnet_id)
+            cache.add_subnet(sub)
+            next if sub.tags.empty?
+
+            sub.tags.each do |tag|
+              if tag[:key].eql?('Name')
+                cache.add_by_tag(tag[:value], sub.subnet_id)
+                break
+              end
             end
           end
         end
 
         id_key = subnet_id.to_sym
-        return subnet_id if subnet_id.start_with?('subnet-') && cache.has_subnet?(id_key)
-        return cache.subnet_by_cidr(id_key) if cache.is_cidr?(subnet_id) && cache.by_cidr.key?(id_key)
-        cache.subnet_by_tag(id_key)
+        return subnet(subnet_id) if subnet_id.start_with?('subnet-') && cache.has_subnet?(id_key)
+        return subnet(cache.subnet_by_cidr(id_key)) if cache.is_cidr?(subnet_id) && cache.by_cidr.key?(id_key)
+        subnet(cache.subnet_by_tag(id_key))
       end
     end
 
@@ -76,6 +82,13 @@ module Awspec::Helper
                                           filters: [{ name: 'vpc-id', values: [vpc_id] }]
                                         })
       res.subnets
+    end
+
+    def subnet(subnet_id)
+      res = ec2_client.describe_subnets({
+                                          filters: [{ name: 'subnet-id', values: [subnet_id] }]
+                                        })
+      res.subnets.single_resource(subnet_id)
     end
   end
 end

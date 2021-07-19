562427b8421e8964b1f458a344a8478020ce4d9c
alceu.freitas <alceu.freitas@semantix.com.br>
2021-07-18 21:31:06 -0300

    feat: extended spec

--- spec/type/eks_nodegroup_spec.rb
+++ spec/type/eks_nodegroup_spec.rb
@@ -5,4 +5,6 @@ describe eks_nodegroup('my-eks-nodegroup'), cluster: 'my-cluster' do
   it { should exist }
   its(:version) { should eq '1.17' }
   it { should be_active }
+  its(:nodegroup_arn) { should eq 'arn:aws:eks:us-west-2:012345678910:nodegroup/my-cluster/my-nodegroup/08bd000a' }
+  its(:node_role) { should eq 'arn:aws:iam::012345678910:role/eks-nodegroup-role' }
 end

6be52d74389780bc062d29dfa5ddf52b2d470395
Alceu Rodrigues de Freitas Junior <glasswalk3r@yahoo.com.br>
2021-08-23 01:14:57 -0300

    feat: custom description for be_opened

--- lib/awspec/matcher/be_opened.rb
+++ lib/awspec/matcher/be_opened.rb
@@ -1,6 +1,15 @@
+DEFAULT_PROTOCOL = '-1'
+DEFAULT_ROUTE = '0.0.0.0/0'
+
 RSpec::Matchers.define :be_opened do |port|
   match do |sg|
     sg.opened?(port, @protocol, @cidr)
+  end
+
+  description do
+    @protocol = DEFAULT_PROTOCOL if @protocol.nil?
+    @cidr = DEFAULT_ROUTE if @cidr.nil?
+    "to be opened on port #{port}, protocol \"#{@protocol}\", to/from #{@cidr}"
   end
 
   chain :protocol do |protocol|

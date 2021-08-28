f7043d2727f1ed696dc09c3af287b70dadde8585
alceu.freitas <alceu.freitas@semantix.com.br>
2021-08-28 00:13:05 -0300

    refactor: sharing constants with stub

--- spec/type/sns_topic_spec.rb
+++ spec/type/sns_topic_spec.rb
@@ -1,10 +1,13 @@
 require 'spec_helper'
-Awspec::Stub.load 'sns_topic'
 
 OWNER = '123456789'
 REGION = 'us-east-1'
 TOPIC_ARN = "arn:aws:sns:#{REGION}:#{OWNER}:foobar"
 SUBSCRIBED = "arn:aws:sns:#{REGION}:#{OWNER}:Foobar:3dbf4999-b3e2-4345-bd11-c34c9784ecca"
+ENDPOINT = "arn:aws:lambda:#{REGION}:#{OWNER}:function:foobar"
+DISPLAY_NAME = 'Useless'
+
+Awspec::Stub.load 'sns_topic'
 
 describe sns_topic(TOPIC_ARN) do
   it { should exist }
@@ -12,7 +15,7 @@ describe sns_topic(TOPIC_ARN) do
   its(:confirmed_subscriptions) { should eq 1 }
   its(:pending_subscriptions) { should eq 0 }
   its(:topic_arn) { should eq TOPIC_ARN }
-  its(:display_name) { should eq 'Useless' }
+  its(:display_name) { should eq DISPLAY_NAME }
   its(:deleted_subscriptions) { should eq 0 }
   its(:subscriptions) { should eql([SUBSCRIBED.to_sym]) }
   its(:id) { should eq TOPIC_ARN }
@@ -21,7 +24,7 @@ describe sns_topic(TOPIC_ARN) do
     { protocol: 'lambda',
       owner: OWNER,
       subscription_arn: SUBSCRIBED, # this is required
-      endpoint: "arn:aws:lambda:#{REGION}:#{OWNER}:function:foobar" }
+      endpoint: ENDPOINT }
   end
 
   describe '#include_subscribed' do

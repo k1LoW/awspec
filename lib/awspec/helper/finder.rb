8116b5a92c1ac3cfc03c431e8741961c58df7866
Shaun Verch <githubpublic@shaunverch.com>
2023-03-26 22:06:03 -0400

    Fix linter error

--- lib/awspec/helper/finder.rb
+++ lib/awspec/helper/finder.rb
@@ -176,7 +176,7 @@ module Awspec::Helper
     # We have to set this conditionally after defining `CLIENT_OPTIONS`,
     # because setting the `endpoint` argument to `nil` results in an error from
     # the aws sdk.
-    if ENV.has_key?('aws_custom_endpoint')
+    if ENV.key?('aws_custom_endpoint')
       CLIENT_OPTIONS[:endpoint] = ENV['aws_custom_endpoint']
     end
 

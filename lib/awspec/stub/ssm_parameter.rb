d39d4d70261cf822991507a968704baffa61c2c6
Andreas Jonsson <andreas.j@cheq.ai>
2023-12-20 14:48:43 -0800

    Added pagination support to ssm_parameter

--- lib/awspec/stub/ssm_parameter.rb
+++ lib/awspec/stub/ssm_parameter.rb
@@ -1,18 +1,29 @@
 # frozen_string_literal: true
 
+next_token = 'eyJOZXh0VG9rZW4iOiBudWxsLCAiYm90b190cnVuY2F0ZV9hbW91bnQiOiAxfQ=='
+
 Aws.config[:ssm] = {
   stub_responses: {
-    describe_parameters: {
-      parameters: [
+    describe_parameters: -> (context) {
+      if context.params[:next_token] == next_token
         {
-          name: 'my-parameter',
-          type: 'SecureString',
-          key_id: 'alias/aws/ssm',
-          description: 'Some description',
-          version: 1,
+          parameters: [
+            {
+              name: 'my-parameter',
+              type: 'SecureString',
+              key_id: 'alias/aws/ssm',
+              description: 'Some description',
+              version: 1,
+            }
+          ],
           next_token: nil
         }
-      ]
+      else
+        {
+          parameters: [],
+          next_token: next_token
+        }
+      end
     }
   }
 }

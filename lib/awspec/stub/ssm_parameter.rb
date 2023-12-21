8c7193d540108458938c22e4dad587071b48ca67
Andreas Jonsson <andreas.j@cheq.ai>
2023-12-20 16:33:40 -0800

    lint/test fix

--- lib/awspec/stub/ssm_parameter.rb
+++ lib/awspec/stub/ssm_parameter.rb
@@ -1,29 +1,30 @@
 # frozen_string_literal: true
 
-next_token = 'eyJOZXh0VG9rZW4iOiBudWxsLCAiYm90b190cnVuY2F0ZV9hbW91bnQiOiAxfQ=='
+def describe_parameters_response(context)
+  next_token = 'eyJOZXh0VG9rZW4iOiBudWxsLCAiYm90b190cnVuY2F0ZV9hbW91bnQiOiAxfQ=='
+  if context.params[:next_token] == next_token
+    {
+      parameters: [
+        {
+          name: 'my-parameter',
+          type: 'SecureString',
+          key_id: 'alias/aws/ssm',
+          description: 'Some description',
+          version: 1
+        }
+      ],
+      next_token: nil
+    }
+  else
+    {
+      parameters: [],
+      next_token: next_token
+    }
+  end
+end
 
 Aws.config[:ssm] = {
   stub_responses: {
-    describe_parameters: lambda { |context|
-      if context.params[:next_token] == next_token
-        {
-          parameters: [
-            {
-              name: 'my-parameter',
-              type: 'SecureString',
-              key_id: 'alias/aws/ssm',
-              description: 'Some description',
-              version: 1
-            }
-          ],
-          next_token: nil
-        }
-      else
-        {
-          parameters: [],
-          next_token: next_token
-        }
-      end
-    }
+    describe_parameters: ->(context) { describe_parameters_response(context) }
   }
 }
